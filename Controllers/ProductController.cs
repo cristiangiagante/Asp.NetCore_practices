27e3d80a5e0e45e2a65b63ec943f289c31a7ea7d
Cristian Giagante <cristiangiagante@hotmail.com>
2018-03-18 18:57:56 -0300

    Add repo, dependecy injection/IoC containerand Details action method/View

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,9 +8,21 @@ namespace aspnetcore_hello.Controllers
 {
     public class ProductController : Controller
     {
+        IRepository _Repository;
+
+        public ProductController(IRepository repository)
+        {
+            _Repository = repository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var products = _Repository.GetProducts();
+            return View(products);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = _Repository.GetProductById(id);
+            return View(product);
         }
     }
 }
---
+++ Services/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcore_hello.Models;
+
+namespace aspnetcore_hello
+{
+    public interface IRepository
+    {
+        List<Product> GetProducts();
+        Product GetProductById(int id);
+        
+    }
+}
